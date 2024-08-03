bd17cbb5773a58abdde5174bf189e4f44f787970
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-08-03 19:13:38 +0530

    feat: add routes for index, about, contact-me, and 404 pages

---
+++ index.js
@@ -0,0 +1,40 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+
+  res.setHeader("content-Type", "Text/html");
+
+  let path = "./";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/contact-me":
+      path += "contact-me.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end;
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
+});
+server.listen(8080, () => console.log("Server Started"));
